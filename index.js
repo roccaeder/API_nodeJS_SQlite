f2ce5ce29e6b3def92c42231504994c73cff90b5
eder <edra1791@gmail.com>
2021-11-23 12:09:51 -0500

    add routes and  get post delete request

--- index.js
+++ index.js
@@ -6,11 +6,50 @@ const app = express();
 
 sequelize.sync({ force: true }).then(() => console.log('start db'))
 
+app.use(express.json());
+
+const carsData = [
+    {
+        "brand":"Toyota",
+        "model": "Corolla",
+        "year": 2020,
+        "color": "red",
+        "price": 9000
+    }
+];
+
 app.post('/cars', (req, res) => {
     Car.create(req.body).then(()=>{
       res.send({success: "Car created"});
     })
 })
+
+app.get('/cars', (req, res) => {
+    Car.findAll().then((data)=>{
+        res.send(data);
+    })
+})
+
+app.get('/cars/:id', async (req, res) => {
+    const id = req.params.id;
+    const car = await Car.findOne({where: {id: id}});
+    res.send(car);
+  })
+  
+  app.put('/cars/:id', async (req, res) => {
+    const id = req.params.id;
+    const body = req.body;
+    const car = await Car.findOne({where: {id: id}});
+    await car.update(body);
+    await car.save();
+    res.send(car);
+  })
+  
+  app.delete('/cars/:id', async (req, res) => {
+    const id = req.params.id;
+    await Car.destroy({where: {id: id}});
+    res.send({success: "Car deleted"});
+  })
 
 app.listen(3001, ()=> {
     console.log('Server is running on port 3001');
